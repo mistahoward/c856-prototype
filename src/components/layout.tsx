bfa7a970fd1d082e41c7b8084c7916d3e49c631f
Alex Howard <alex.howard@powershades.com>
2023-11-13 16:46:45 -0600

    linting

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,53 +1,46 @@
 import React, { ReactNode } from 'react';
-import { Button, Col, Form, Nav, Navbar, Row } from 'react-bootstrap';
+import {
+	Button, Col, Form, Nav, Navbar, Row
+} from 'react-bootstrap';
 
-const Layout = ({ children }: { children: ReactNode }) => {
-	console.debug();
-	return (
-		<>
-			<Navbar expand="lg" id="header" className="bg-body-secondary">
-				<Navbar.Brand href="/">
+const Layout = ({ children }: { children: ReactNode }) => (
+	<>
+		<Navbar expand="lg" id="header" className="bg-body-secondary">
+			<Navbar.Brand href="/">
+				<Row>
+					<Col className="ms-2">TT</Col>
+					<Col>Tropical Taniti</Col>
+				</Row>
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="me-auto">
+					<Nav.Link href="/About">About</Nav.Link>
+					<Nav.Link href="/Destinations">Destinations</Nav.Link>
+					<Nav.Link href="/Accommodations">Accommodations</Nav.Link>
+					<Nav.Link href="/Reviews">Reviews</Nav.Link>
+				</Nav>
+				<Form className="me-2">
 					<Row>
-						<Col className="ms-2">TT</Col>
-						<Col>Tropical Taniti</Col>
+						<Col xs="auto">
+							<Form.Control type="text" placeholder="Search" className="me-sm-2" />
+						</Col>
+						<Col>
+							<Button variant="outline-secondary" type="submit">
+								Submit
+							</Button>
+						</Col>
 					</Row>
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-						<Nav.Link href="/About">About</Nav.Link>
-						<Nav.Link href="/Destinations">Destinations</Nav.Link>
-						<Nav.Link href="/Accommodations">Accommodations</Nav.Link>
-						<Nav.Link href="/Reviews">Reviews</Nav.Link>
-					</Nav>
-					<Form className="me-2">
-						<Row>
-							<Col xs="auto">
-								<Form.Control
-									type="text"
-									placeholder="Search"
-									className="me-sm-2"
-								/>
-							</Col>
-							<Col>
-								<Button variant="outline-secondary" type="submit">
-									Submit
-								</Button>
-							</Col>
-						</Row>
-					</Form>
-				</Navbar.Collapse>
-			</Navbar>
-			{children}
-			<Navbar id="footer" className="bg-body-secondary">
-				<Navbar.Collapse className="justify-content-center">
-					<Navbar.Text>
-						Copyright 2023
-					</Navbar.Text>
-				</Navbar.Collapse>
-			</Navbar>
-		</>
-	);
-};
+				</Form>
+			</Navbar.Collapse>
+		</Navbar>
+		{children}
+		<Navbar id="footer" className="bg-body-secondary">
+			<Navbar.Collapse className="justify-content-center">
+				<Navbar.Text>Copyright 2023</Navbar.Text>
+			</Navbar.Collapse>
+		</Navbar>
+	</>
+);
 
 export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,223 +1,219 @@
 import React, { FC } from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
-import { Container, Carousel, Card, Button, Row, Col } from 'react-bootstrap';
+import {
+	Container, Carousel, Card, Button, Row, Col
+} from 'react-bootstrap';
 import { StaticImage } from 'gatsby-plugin-image';
 
 import Layout from '../components/layout';
 import '../scss/main.scss';
 
-const IndexPage: FC<PageProps> = () => {
-	console.debug();
-	return (
-		<Container fluid id="root">
-			<Layout>
-				<Carousel fade id="main-carousel">
-					<Carousel.Item>
-						<StaticImage src="../images/carousel_1.jpg" alt="Tropical Scene" />
-						<div className="overlay" />
-						<Carousel.Caption>
-							<h3>Your Palm-Shadowed Sanctuary Awaits</h3>
-							<p>
-								Photo by &nbsp;
-								<a href="https://unsplash.com/@itte?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-									Ittemaldiviano 🇲🇻
-								</a>
-								&nbsp;on&nbsp;
-								<a href="https://unsplash.com/photos/green-palm-tree-on-white-sand-beach-during-daytime-jmkMl20jNS0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-									Unsplash
-								</a>
-							</p>
-						</Carousel.Caption>
-					</Carousel.Item>
-					<Carousel.Item>
-						<StaticImage
-							src="../images/carousel_2.jpg"
-							alt="Coconut Trees and Body of Water"
-						/>
-						<div className="overlay" />
-						<Carousel.Caption>
-							<h3>Serenity in Shades of Blue</h3>
-							<p>
-								Photo by &nbsp;
-								<a href="https://unsplash.com/@nickkane?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-									Nick Kane
-								</a>
-								&nbsp;on&nbsp;
-								<a href="https://unsplash.com/photos/coconut-trees-and-body-of-water-painting-eaI3VpRnMbw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-									Unsplash
-								</a>
-							</p>
-						</Carousel.Caption>
-					</Carousel.Item>
-					<Carousel.Item>
-						<StaticImage
-							src="../images/carousel_3.jpg"
-							alt="Tropical Cliff line with a village"
-						/>
-						<div className="overlay" />
-						<Carousel.Caption>
-							<h3>Coastal Charm, Sunlit Dreams</h3>
-							<p>
-								Photo by&nbsp;
-								<a href="https://unsplash.com/@nathan_cima?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-									Nathan Cima
-								</a>
-								&nbsp;on&nbsp;
-								<a href="https://unsplash.com/photos/a-view-of-a-beach-with-houses-on-the-cliff-2KZksT2BSDw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-									Unsplash
-								</a>
-							</p>
-						</Carousel.Caption>
-					</Carousel.Item>
-				</Carousel>
-				<div className="image-card-container">
-					<Card>
-						<Card.Body>
-							<Card.Title>Card Title</Card.Title>
-							<Card.Text>
-								Some quick example text to build on the card title and make up the
-								bulk of the card&apos;s content.
-							</Card.Text>
-							<Button variant="primary">Go somewhere</Button>
-						</Card.Body>
-					</Card>
+const IndexPage: FC<PageProps> = () => (
+	<Container fluid id="root">
+		<Layout>
+			<Carousel fade id="main-carousel">
+				<Carousel.Item>
+					<StaticImage src="../images/carousel_1.jpg" alt="Tropical Scene" />
+					<div className="overlay" />
+					<Carousel.Caption>
+						<h3>Your Palm-Shadowed Sanctuary Awaits</h3>
+						<p>
+							Photo by &nbsp;
+							<a href="https://unsplash.com/@itte?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+								Ittemaldiviano 🇲🇻
+							</a>
+							&nbsp;on&nbsp;
+							<a href="https://unsplash.com/photos/green-palm-tree-on-white-sand-beach-during-daytime-jmkMl20jNS0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+								Unsplash
+							</a>
+						</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
 					<StaticImage
-						src="../images/lady_1.jpg"
-						alt="Lady on a Beach holding a drink"
+						src="../images/carousel_2.jpg"
+						alt="Coconut Trees and Body of Water"
 					/>
-					<div className="attribution-overlay">
-						Photo by&nbsp;
-						<a href="https://unsplash.com/@emilybauman__?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-							Emily Bauman
-						</a>
-						&nbsp;on&nbsp;
-						<a href="https://unsplash.com/photos/person-holding-coconut-fnAN6OFXR9A?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-							Unsplash
-						</a>
-					</div>
-				</div>
-				<Container className="mt-4 mb-4">
-					<Row>
-						<h1 className="display-4">Destinations</h1>
-					</Row>
-					<Row>
-						<Col>
-							<Card>
-								<Card.Img variant="top" src="https://placehold.co/100x180" />
-								<Card.Body>
-									<Card.Title>Card Title</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make
-										up the bulk of the card&apos;s content.
-									</Card.Text>
-									<Button variant="primary">Go somewhere</Button>
-								</Card.Body>
-							</Card>
-						</Col>
-						<Col>
-							<Card>
-								<Card.Img variant="top" src="https://placehold.co/100x180" />
-								<Card.Body>
-									<Card.Title>Card Title</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make
-										up the bulk of the card&apos;s content.
-									</Card.Text>
-									<Button variant="primary">Go somewhere</Button>
-								</Card.Body>
-							</Card>
-						</Col>
-						<Col>
-							<Card>
-								<Card.Img variant="top" src="https://placehold.co/100x180" />
-								<Card.Body>
-									<Card.Title>Card Title</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make
-										up the bulk of the card&apos;s content.
-									</Card.Text>
-									<Button variant="primary">Go somewhere</Button>
-								</Card.Body>
-							</Card>
-						</Col>
-					</Row>
-				</Container>
-				<div className="image-card-container">
-					<Card>
-						<Card.Body>
-							<Card.Title>Card Title</Card.Title>
-							<Card.Text>
-								Some quick example text to build on the card title and make up the
-								bulk of the card&apos;s content.
-							</Card.Text>
-							<Button variant="primary">Go somewhere</Button>
-						</Card.Body>
-					</Card>
+					<div className="overlay" />
+					<Carousel.Caption>
+						<h3>Serenity in Shades of Blue</h3>
+						<p>
+							Photo by &nbsp;
+							<a href="https://unsplash.com/@nickkane?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+								Nick Kane
+							</a>
+							&nbsp;on&nbsp;
+							<a href="https://unsplash.com/photos/coconut-trees-and-body-of-water-painting-eaI3VpRnMbw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+								Unsplash
+							</a>
+						</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
 					<StaticImage
-						src="../images/drink_1.jpg"
-						alt="Lady on a Beach holding a drink"
+						src="../images/carousel_3.jpg"
+						alt="Tropical Cliff line with a village"
 					/>
-					<div className="attribution-overlay">
-						Photo by&nbsp;
-						<a href="https://unsplash.com/@kaizennguyen?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-							Kaizen Nguyễn
-						</a>
-						&nbsp;on&nbsp;
-						<a href="https://unsplash.com/photos/six-clear-glass-mason-jars-filled-with-juice-on-black-table-jcLcWL8D7AQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-							Unsplash
-						</a>
-					</div>
+					<div className="overlay" />
+					<Carousel.Caption>
+						<h3>Coastal Charm, Sunlit Dreams</h3>
+						<p>
+							Photo by&nbsp;
+							<a href="https://unsplash.com/@nathan_cima?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+								Nathan Cima
+							</a>
+							&nbsp;on&nbsp;
+							<a href="https://unsplash.com/photos/a-view-of-a-beach-with-houses-on-the-cliff-2KZksT2BSDw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+								Unsplash
+							</a>
+						</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
+			<div className="image-card-container">
+				<Card>
+					<Card.Body>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card&apos;s content.
+						</Card.Text>
+						<Button variant="primary">Go somewhere</Button>
+					</Card.Body>
+				</Card>
+				<StaticImage src="../images/lady_1.jpg" alt="Lady on a Beach holding a drink" />
+				<div className="attribution-overlay">
+					Photo by&nbsp;
+					<a href="https://unsplash.com/@emilybauman__?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+						Emily Bauman
+					</a>
+					&nbsp;on&nbsp;
+					<a href="https://unsplash.com/photos/person-holding-coconut-fnAN6OFXR9A?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+						Unsplash
+					</a>
 				</div>
-				<Container className="mt-4 mb-4">
-					<Row>
-						<h1 className="display-4">Accommodations</h1>
-					</Row>
-					<Row>
-						<Col>
-							<Card>
-								<Card.Img variant="top" src="https://placehold.co/100x180" />
-								<Card.Body>
-									<Card.Title>Card Title</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make
-										up the bulk of the card&apos;s content.
-									</Card.Text>
-									<Button variant="primary">Go somewhere</Button>
-								</Card.Body>
-							</Card>
-						</Col>
-						<Col>
-							<Card>
-								<Card.Img variant="top" src="https://placehold.co/100x180" />
-								<Card.Body>
-									<Card.Title>Card Title</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make
-										up the bulk of the card&apos;s content.
-									</Card.Text>
-									<Button variant="primary">Go somewhere</Button>
-								</Card.Body>
-							</Card>
-						</Col>
-						<Col>
-							<Card>
-								<Card.Img variant="top" src="https://placehold.co/100x180" />
-								<Card.Body>
-									<Card.Title>Card Title</Card.Title>
-									<Card.Text>
-										Some quick example text to build on the card title and make
-										up the bulk of the card&apos;s content.
-									</Card.Text>
-									<Button variant="primary">Go somewhere</Button>
-								</Card.Body>
-							</Card>
-						</Col>
-					</Row>
-				</Container>
-			</Layout>
-		</Container>
-	);
-};
+			</div>
+			<Container className="mt-4 mb-4">
+				<Row>
+					<h1 className="display-4">Destinations</h1>
+				</Row>
+				<Row>
+					<Col>
+						<Card>
+							<Card.Img variant="top" src="https://placehold.co/100x180" />
+							<Card.Body>
+								<Card.Title>Card Title</Card.Title>
+								<Card.Text>
+									Some quick example text to build on the card title and make
+									up the bulk of the card&apos;s content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Card>
+							<Card.Img variant="top" src="https://placehold.co/100x180" />
+							<Card.Body>
+								<Card.Title>Card Title</Card.Title>
+								<Card.Text>
+									Some quick example text to build on the card title and make
+									up the bulk of the card&apos;s content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Card>
+							<Card.Img variant="top" src="https://placehold.co/100x180" />
+							<Card.Body>
+								<Card.Title>Card Title</Card.Title>
+								<Card.Text>
+									Some quick example text to build on the card title and make
+									up the bulk of the card&apos;s content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+			<div className="image-card-container">
+				<Card>
+					<Card.Body>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card&apos;s content.
+						</Card.Text>
+						<Button variant="primary">Go somewhere</Button>
+					</Card.Body>
+				</Card>
+				<StaticImage
+					src="../images/drink_1.jpg"
+					alt="Lady on a Beach holding a drink"
+				/>
+				<div className="attribution-overlay">
+					Photo by&nbsp;
+					<a href="https://unsplash.com/@kaizennguyen?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+						Kaizen Nguyễn
+					</a>
+					&nbsp;on&nbsp;
+					<a href="https://unsplash.com/photos/six-clear-glass-mason-jars-filled-with-juice-on-black-table-jcLcWL8D7AQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+						Unsplash
+					</a>
+				</div>
+			</div>
+			<Container className="mt-4 mb-4">
+				<Row>
+					<h1 className="display-4">Accommodations</h1>
+				</Row>
+				<Row>
+					<Col>
+						<Card>
+							<Card.Img variant="top" src="https://placehold.co/100x180" />
+							<Card.Body>
+								<Card.Title>Card Title</Card.Title>
+								<Card.Text>
+									Some quick example text to build on the card title and make
+									up the bulk of the card&apos;s content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Card>
+							<Card.Img variant="top" src="https://placehold.co/100x180" />
+							<Card.Body>
+								<Card.Title>Card Title</Card.Title>
+								<Card.Text>
+									Some quick example text to build on the card title and make
+									up the bulk of the card&apos;s content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col>
+						<Card>
+							<Card.Img variant="top" src="https://placehold.co/100x180" />
+							<Card.Body>
+								<Card.Title>Card Title</Card.Title>
+								<Card.Text>
+									Some quick example text to build on the card title and make
+									up the bulk of the card&apos;s content.
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+		</Layout>
+	</Container>
+);
 
 export default IndexPage;
 
